bce5af11c484e63420e57bf1831844e7ca3e48c6
Simone Nicol <42026938+en0mia@users.noreply.github.com>
2026-01-24 11:48:34 +0100

    Add utils to convert to MR.

--- Source/utils.c
+++ Source/utils.c
@@ -1,3 +1,16 @@
 #include "utils.h"
 
 //	here you can add the functions that use during the simulation or the exam
+
+// === TIMERS ===
+uint32_t s_to_mr(uint32_t seconds, uint32_t timer_freq, uint32_t prescaler) {
+    return (seconds * timer_freq) / (prescaler + 1);
+}
+
+uint32_t ms_to_mr(uint32_t ms, uint32_t timer_freq, uint32_t prescaler) {
+    return ((ms / 1000) * timer_freq) / (prescaler + 1);
+}
+
+uint32_t hz_to_mr(uint32_t hz, uint32_t timer_freq, uint32_t prescaler) {
+    return timer_freq / ((prescaler + 1) * hz);
+}
--- Source/utils.h
+++ Source/utils.h
@@ -1,2 +1,6 @@
 #include "sample.h"
 #include "functions.h"	
+
+uint32_t s_to_mr(uint32_t seconds, uint32_t timer_freq, uint32_t prescaler);
+uint32_t hz_to_mr(uint32_t hz, uint32_t timer_freq, uint32_t prescaler);
+uint32_t ms_to_mr(uint32_t ms, uint32_t timer_freq, uint32_t prescaler);
